c5952a4977b21de0e6739bdd71666ed7440d01a6
Austin Matthews <114279644+ausmatths@users.noreply.github.com>
2025-06-06 22:42:55 -0700

    Increase logo size in Navbar component for better visibility

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-3 h-3 object-contain' />
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>
             Austin <span className='sm:block hidden'>| Computer Science</span>
           </p> 
